62a6f41b652e3fa938bdb967402d2d8c390e1be6
alvdela <alvarodlr002@gmail.com>
2024-03-01 15:16:06 +0100

    Operadores de calculo

--- app/src/main/java/com/alvdela/conceptosbasicos/MainActivity.kt
+++ app/src/main/java/com/alvdela/conceptosbasicos/MainActivity.kt
@@ -13,12 +13,44 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        /*
         val fecha = "05/06/2001"
-        var nombre = "Alvaro"
+        var nombre: String = "Alvaro"
+        var vip: Boolean = false
 
-        println(nombre)
+        var saludo = "Hola $nombre"
+
+        println(saludo)
         //fecha = "03/02/1992" -> Los valores (val) no pueden modificarse
+        */
+
+        val a:Int = 5 + 5 //10
+        val b:Int = 10 - 2 //8
+        val c:Int = 3 * 4 //12
+        val d:Int = 10 / 5 //2
+        val e:Int = 10 % 3 //1 = Resto
+        val f:Int = 10 / 6 //Division periodica, solo se mantiene parte entera 1
+
+        var aPreIncremento: Int = 5
+        var bPreDecremento: Int = 5
+        var cPostIncremento: Int = 5
+        var dPostDecremento: Int = 5
+
+        println(aPreIncremento)
+        println(++aPreIncremento)
+        println(aPreIncremento)
+
+        println(bPreDecremento)
+        println(--bPreDecremento)
+        println(bPreDecremento)
+
+        println(cPostIncremento)
+        println(cPostIncremento++)
+        println(cPostIncremento)
+
+        println(dPostDecremento)
+        println(dPostDecremento--)
+        println(dPostDecremento)
 
     }
 }
