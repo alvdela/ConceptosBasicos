5d251527f2a524845a6cfacf1f16294dac23dd83
alvdela <alvarodlr002@gmail.com>
2024-03-01 13:49:42 +0100

    Variables, Valores y Constantes

--- app/src/main/java/com/alvdela/conceptosbasicos/MainActivity.kt
+++ app/src/main/java/com/alvdela/conceptosbasicos/MainActivity.kt
@@ -5,8 +5,20 @@ import androidx.activity.ComponentActivity
 
 
 class MainActivity : ComponentActivity() {
+
+    companion object{
+        //Ambido distinto a la creación de la ventana
+        const val moneda = "EUR"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fecha = "05/06/2001"
+        var nombre = "Alvaro"
+
+        println(nombre)
+        //fecha = "03/02/1992" -> Los valores (val) no pueden modificarse
+
     }
 }
